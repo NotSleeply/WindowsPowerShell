d32feda2b8ab076d48212411037967536c020baa
NotSleeply <155351298+NotSleeply@users.noreply.github.com>
2025-09-26 18:33:51 +0800

    first

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,31 @@
+function Get-DetailedChildItem { Get-ChildItem -Force }
+
+New-Alias -Name ll -Value Get-DetailedChildItem
+New-Alias -Name grep -Value Select-String
+New-Alias -Name redis -Value "D:\program files\redis\redis-server.exe"
+New-Alias -Name fuck -Value tldr
+
+function ifconfig {
+    ipconfig /all
+}
+function ip {
+    ipconfig /all
+}
+function redis-cli {
+    & "D:\program files\redis\redis-cli.exe" --raw @args
+}
+function ag {
+    ollama run deepseek-v3.1:671b-cloud @Args
+}
+function ag-coder {
+    ollama run qwen3-coder:480b-cloud @Args
+}
+function ag-gpt {
+    ollama run gpt-oss:120b-cloud @Args
+}
+function ag-q {
+    ollama run qwen3:latest @Args
+}
+function ag-dp {
+    ollama run deepseek-r1:latest @Args
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# [WindowsPowerShell 配置](https://github.com/NotSleeply/WindowsPowerShell)
+
+本项目包含[个人](https://github.com/NotSleeply) PowerShell 配置文件和常用命令别名，旨在提升命令行操作效率。
+
+## 使用方法
+
+-  将 `Microsoft.PowerShell_profile.ps1` 放置到 PowerShell 配置目录（如 `C:\Users\<用户名>\Documents\WindowsPowerShell\`）。
